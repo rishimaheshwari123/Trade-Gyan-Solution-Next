f1224451b6d4df8073eace98c5256caebec9ecc5
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-02-28 12:17:10 +0530

    error solve

--- src/components/investor/core/home/ContentTwo.jsx
+++ src/components/investor/core/home/ContentTwo.jsx
@@ -1,51 +1,171 @@
-import React from 'react';
+import React from "react";
 
 const GrayTextComponent = () => {
   return (
     <div className="bg-white text-gray-700 p-6">
-      <h1 className="text-gray-900 font-bold text-2xl">SEBI Registered Company: Trade Gyan Solutions</h1>
-      <p>At Trade Gyan Solutions, we help investors. We provide advanced insights, custom services, and expert guidance. We are a SEBI registered company in India. We serve veteran traders and new investors. We take pride in being a SEBI Registered Company. We provide reliable and transparent investment advisory services. Our SEBI registration shows our commitment to high standards. We offer many investment solutions. These include equity baskets, PMS services, Nifty and Banknifty options, institutional equity, and private equity. We are a SEBI-registered company. This means we uphold ethics, rules, and customer satisfaction. We have a range of investment solutions. These include equity baskets, PMS, Nifty and Banknifty options, institutional equity, and private equity. Our SEBI registration gives clients peace of mind. It shows we take care of their financial goals. We can guide you whether you are an individual, high-net-worth, or institutional client. We can help you reach your investment goals. Call us now for services from a reliable SEBI Registered Company focused on your prosperity.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">SEBI Registered Investment Advisor</h2>
-      <p>Trade Gyan Solutions helps investors understand financial markets better. We are a trusted advisory service registered with SEBI. We provide customized investment plans based on your goals. Our expert team offers advice on stocks, commodities, forex, and private equity. We make sure each choice is supported by research and analysis. If you want to build a diverse portfolio, learn about Nifty and Banknifty options, or invest in unlisted shares, Trade Gyan Solutions is your trusted partner for long-term financial success.</p>
-      
+      <h1 className="text-gray-900 font-bold text-2xl">
+        SEBI Registered Company: Trade Gyan Solutions
+      </h1>
+      <p>
+        At Trade Gyan Solutions, we help investors. We provide advanced
+        insights, custom services, and expert guidance. We are a SEBI registered
+        company in India. We serve veteran traders and new investors. We take
+        pride in being a SEBI Registered Company. We provide reliable and
+        transparent investment advisory services. Our SEBI registration shows
+        our commitment to high standards. We offer many investment solutions.
+        These include equity baskets, PMS services, Nifty and Banknifty options,
+        institutional equity, and private equity. We are a SEBI-registered
+        company. This means we uphold ethics, rules, and customer satisfaction.
+        We have a range of investment solutions. These include equity baskets,
+        PMS, Nifty and Banknifty options, institutional equity, and private
+        equity. Our SEBI registration gives clients peace of mind. It shows we
+        take care of their financial goals. We can guide you whether you are an
+        individual, high-net-worth, or institutional client. We can help you
+        reach your investment goals. Call us now for services from a reliable
+        SEBI Registered Company focused on your prosperity.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        SEBI Registered Investment Advisor
+      </h2>
+      <p>
+        Trade Gyan Solutions helps investors understand financial markets
+        better. We are a trusted advisory service registered with SEBI. We
+        provide customized investment plans based on your goals. Our expert team
+        offers advice on stocks, commodities, forex, and private equity. We make
+        sure each choice is supported by research and analysis. If you want to
+        build a diverse portfolio, learn about Nifty and Banknifty options, or
+        invest in unlisted shares, Trade Gyan Solutions is your trusted partner
+        for long-term financial success.
+      </p>
+
       <h2 className="text-gray-800 font-semibold text-xl mt-4">Facilities</h2>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Nifty Option & Banknifty Option Trading</h2>
-      <p>Nifty Option and Banknifty Option trading can make a lot of money if done right. To make safe trades, join a company registered with SEBI. Professional advisors will give real-time information and expert analysis. This will help you make good choices to earn returns safely. Being with a SEBI registered company like Trade Gyan Solutions ensures privacy, ethical practices, and expert guidance for the best options trading.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Stock Basket & Share Reports</h2>
-      <p>A stock basket is a group of stocks. This lets you track several stocks together. A share report shows information about a stock. It has details like the stock price and company details. Tracking a basket of stocks and their reports can help make investing decisions. You can see how different stocks perform compared to each other.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Institutional Reports</h2>
-      <p>Institutions make reports. These reports help people understand how the institution is doing. The reports give information about the institution's activities and performance. They help show if the institution is meeting its goals. Reading the reports lets people see how the institution is working.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Stock Market Tomorrow View</h2>
-      <p>Our SEBI registered company provides timely and accurate market insights. We study market trends, technical levels, and institutional activities. This helps us predict the likely direction for the next day's trading. Our expert advice allows investors to make smart decisions. This helps them stay ahead of market changes and find profitable opportunities.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">PMS Services & Investment Portfolios</h2>
-      <p>Investment portfolios allow you to invest in different assets. PMS Services provides professional investment management. This can help you reach your financial goals. Portfolio management services pick investments that fit your risk tolerance. They monitor your portfolio and make changes when needed. PMS Services can invest in stocks, bonds, and other assets on your behalf.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Investment in Crude Oil & Gold</h2>
-      <p>Crude oil and gold are popular investments. Investing in these can be risky but also profitable. Crude oil prices can change a lot. Its price depends on many factors. Gold is also a common investment. Its price also changes often. Many people buy gold as a safe investment.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Equity Basket & Investment in Equity</h2>
-      <p>Equity baskets allow you to invest in a group of stocks. This gives you exposure to different companies. Investing in an equity basket can be a good way to build a portfolio. It helps balance your risk across various stocks.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">MCX & NCDEX Services</h2>
-      <p>They let people buy and sell commodities like gold, silver, and oil. Using these services, you can trade these items. You don't have to own the actual products. Instead, you can speculate on their prices going up or down.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Forex and Unlisted Shares Services</h2>
-      <p>Expand your investment options with our trading services. We offer foreign exchange and limited non-listed stock opportunities. Our portfolio helps diversify your investments. This opens new growth paths for you.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Institutional and Private Equity Solutions</h2>
-      <p>Gain an edge in institutional and private equity investments. Our team helps you find profitable chances. We also give strategic advice to get the best returns.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">Hire Investment Advisors</h2>
-      <p>Looking for advice? We have experienced investment advisors. They can design an approach made just for you. It will fit your needs and risk level.</p>
-      
-      <h2 className="text-gray-800 font-semibold text-xl mt-4">NRI Investment Services</h2>
-      <p>We help non-resident Indians (NRIs) who find investing in India's markets hard. We can help with stocks, real estate, and other investments. We make it easy for NRIs to invest in India.</p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Nifty Option & Banknifty Option Trading
+      </h2>
+      <p>
+        Nifty Option and Banknifty Option trading can make a lot of money if
+        done right. To make safe trades, join a company registered with SEBI.
+        Professional advisors will give real-time information and expert
+        analysis. This will help you make good choices to earn returns safely.
+        Being with a SEBI registered company like Trade Gyan Solutions ensures
+        privacy, ethical practices, and expert guidance for the best options
+        trading.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Stock Basket & Share Reports
+      </h2>
+      <p>
+        A stock basket is a group of stocks. This lets you track several stocks
+        together. A share report shows information about a stock. It has details
+        like the stock price and company details. Tracking a basket of stocks
+        and their reports can help make investing decisions. You can see how
+        different stocks perform compared to each other.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Institutional Reports
+      </h2>
+      <p>
+        Institutions make reports. These reports help people understand how the
+        institution is doing. The reports give information about the
+        institutions activities and performance. They help show if the
+        institution is meeting its goals. Reading the reports lets people see
+        how the institution is working.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Stock Market Tomorrow View
+      </h2>
+      <p>
+        Our SEBI registered company provides timely and accurate market
+        insights. We study market trends, technical levels, and institutional
+        activities. This helps us predict the likely direction for the next
+        days trading. Our expert advice allows investors to make smart
+        decisions. This helps them stay ahead of market changes and find
+        profitable opportunities.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        PMS Services & Investment Portfolios
+      </h2>
+      <p>
+        Investment portfolios allow you to invest in different assets. PMS
+        Services provides professional investment management. This can help you
+        reach your financial goals. Portfolio management services pick
+        investments that fit your risk tolerance. They monitor your portfolio
+        and make changes when needed. PMS Services can invest in stocks, bonds,
+        and other assets on your behalf.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Investment in Crude Oil & Gold
+      </h2>
+      <p>
+        Crude oil and gold are popular investments. Investing in these can be
+        risky but also profitable. Crude oil prices can change a lot. Its price
+        depends on many factors. Gold is also a common investment. Its price
+        also changes often. Many people buy gold as a safe investment.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Equity Basket & Investment in Equity
+      </h2>
+      <p>
+        Equity baskets allow you to invest in a group of stocks. This gives you
+        exposure to different companies. Investing in an equity basket can be a
+        good way to build a portfolio. It helps balance your risk across various
+        stocks.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        MCX & NCDEX Services
+      </h2>
+      <p>
+        They let people buy and sell commodities like gold, silver, and oil.
+        Using these services, you can trade these items. You dont have to own
+        the actual products. Instead, you can speculate on their prices going up
+        or down.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Forex and Unlisted Shares Services
+      </h2>
+      <p>
+        Expand your investment options with our trading services. We offer
+        foreign exchange and limited non-listed stock opportunities. Our
+        portfolio helps diversify your investments. This opens new growth paths
+        for you.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Institutional and Private Equity Solutions
+      </h2>
+      <p>
+        Gain an edge in institutional and private equity investments. Our team
+        helps you find profitable chances. We also give strategic advice to get
+        the best returns.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        Hire Investment Advisors
+      </h2>
+      <p>
+        Looking for advice? We have experienced investment advisors. They can
+        design an approach made just for you. It will fit your needs and risk
+        level.
+      </p>
+
+      <h2 className="text-gray-800 font-semibold text-xl mt-4">
+        NRI Investment Services
+      </h2>
+      <p>
+        We help non-resident Indians (NRIs) who find investing in Indias
+        markets hard. We can help with stocks, real estate, and other
+        investments. We make it easy for NRIs to invest in India.
+      </p>
     </div>
   );
 };
